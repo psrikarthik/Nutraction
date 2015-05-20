106d033affddd02e9066a99a1e96d44df7b7cbff
Karthik <SKP@Users-iMac-2.home>
2015-05-20 09:37:22 +0100

    Initial Commit

---
+++ Nutraction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nutraction
+//
+//  Created by Karthik on 20/05/2015.
+//  Copyright (c) 2015 Clykx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
